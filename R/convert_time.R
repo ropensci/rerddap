fd0b2216f0d193f78f56ed2a1de501bba46ffc43
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-24 09:56:13 -0700

    added local convert time option

--- R/convert_time.R
+++ R/convert_time.R
@@ -5,6 +5,9 @@
 #' @param isoTime character; A string time representation.
 #' @param units character; Units to return. Default: "seconds since 1970-01-01T00:00:00Z"
 #' @param url Base URL of the ERDDAP server
+#' @param method (character) One of local or web. Local simply uses \code{as.POSIXct},
+#' while web method uses the ERDDAP time conversion service
+#' \code{/erddap/convert/time.txt}
 #' @param ... Curl args passed on to \code{\link[httr]{GET}}
 #' @examples  \donttest{
 #' convert_time(n = 473472000)
@@ -12,10 +15,16 @@
 #' }
 
 convert_time <- function(n = NULL, isoTime = NULL,
-  units = "seconds since 1970-01-01T00:00:00Z", url="http://coastwatch.pfeg.noaa.gov", ...)
-{
-  args <- rc(list(n=n, isoTime=isoTime, units=units))
-  res <- GET(paste0(pu(url), '/erddap/convert/time.txt'), query = args, ...)
-  stop_for_status(res)
-  content(res, "text")
+  units = "seconds since 1970-01-01T00:00:00Z", url="http://coastwatch.pfeg.noaa.gov",
+  method = "local", ...) {
+
+  if (method == "local") {
+    format(as.POSIXct(n, origin = "1970-01-01T00:00:00Z", tz = "UTC"),
+           format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
+  } else {
+    args <- rc(list(n = n, isoTime = isoTime, units = units))
+    res <- GET(paste0(pu(url), '/erddap/convert/time.txt'), query = args, ...)
+    stop_for_status(res)
+    content(res, "text")
+  }
 }
