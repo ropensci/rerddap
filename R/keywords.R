3ca3f5d130f992a20ca26055f95475a18c2befd3
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-24 17:24:31 -0700

    changed fxn name keywords to key_words to avoid conflict with dataset keywords

--- R/keywords.R
+++ R/keywords.R
@@ -5,18 +5,21 @@
 #' \url{http://cfconventions.org/Data/cf-standard-names/27/build/cf-standard-name-table.html}
 #' @param gcmd character; A GCMD science keyword
 #' \url{http://gcmd.gsfc.nasa.gov/learn/keyword_list.html}
-#' @param url Base URL of the ERDDAP server
+#' @param url A URL for an ERDDAP server. Default: \url{http://upwell.pfeg.noaa.gov/erddap/}
 #' @param ... Curl args passed on to \code{\link[httr]{GET}}
 #' @examples  \donttest{
-#' keywords(cf = "air_pressure")
-#' cat(keywords(cf = "air_pressure"))
-#' keywords(gcmd = "Atmosphere > Atmospheric Pressure > Sea Level Pressure")
-#' cat(keywords(gcmd = "Atmosphere > Atmospheric Pressure > Sea Level Pressure"))
+#' key_words(cf = "air_pressure")
+#' cat(key_words(cf = "air_pressure"))
+#' key_words(gcmd = "Atmosphere > Atmospheric Pressure > Sea Level Pressure")
+#' cat(key_words(gcmd = "Atmosphere > Atmospheric Pressure > Sea Level Pressure"))
+#'
+#' # a different ERDDAP server
+#' key_words(cf = "air_pressure", url = servers()$url[5])
 #' }
 
-keywords <- function(cf = NULL, gcmd = NULL, url="http://coastwatch.pfeg.noaa.gov", ...){
-  args <- rc(list(cf=cf, gcmd=gcmd))
-  res <- GET(paste0(pu(url), '/erddap/convert/keywords.txt'), query = args, ...)
+key_words <- function(cf = NULL, gcmd = NULL, url = eurl(), ...){
+  args <- rc(list(cf = cf, gcmd = gcmd))
+  res <- GET(paste0(pu(url), '/convert/keywords.txt'), query = args, ...)
   stop_for_status(res)
   content(res, "text")
 }
