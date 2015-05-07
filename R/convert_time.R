615f429a52405d9c5d5df523897e821e744265e5
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-07 13:21:41 -0700

    fixes to convert_time and convert_units

--- R/convert_time.R
+++ R/convert_time.R
@@ -1,4 +1,4 @@
-#' Convert a CF Standard Name to/from a GCMD Science Keyword
+#' Convert a UDUNITS compatible time to ISO time
 #'
 #' @export
 #' @param n numeric; A unix time number.
@@ -9,22 +9,44 @@
 #' while web method uses the ERDDAP time conversion service
 #' \code{/erddap/convert/time.txt}
 #' @param ... Curl args passed on to \code{\link[httr]{GET}}
+#' @details When \code{method = "web"} time zone is GMT/UTC
 #' @examples  \donttest{
+#' # local conversions
 #' convert_time(n = 473472000)
 #' convert_time(isoTime = "1985-01-02T00:00:00Z")
+#'
+#' # using an erddap web service
+#' convert_time(n = 473472000, method = "web")
+#' convert_time(isoTime = "1985-01-02T00:00:00Z", method = "web")
 #' }
 
 convert_time <- function(n = NULL, isoTime = NULL,
-  units = "seconds since 1970-01-01T00:00:00Z", url="http://coastwatch.pfeg.noaa.gov",
+  units = "seconds since 1970-01-01T00:00:00Z", url = "http://coastwatch.pfeg.noaa.gov",
   method = "local", ...) {
 
+  if (!is.null(n)) stopifnot(is.numeric(n))
+  if (!is.null(isoTime)) stopifnot(is.character(isoTime))
+  check1notboth(n, isoTime)
   if (method == "local") {
-    format(as.POSIXct(n, origin = "1970-01-01T00:00:00Z", tz = "UTC"),
+    format(as.POSIXct(rc(list(n, isoTime))[[1]], origin = "1970-01-01T00:00:00Z", tz = "UTC"),
            format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
   } else {
     args <- rc(list(n = n, isoTime = isoTime, units = units))
     res <- GET(paste0(pu(url), '/erddap/convert/time.txt'), query = args, ...)
     stop_for_status(res)
     content(res, "text")
+  }
+}
+
+depsub <- function(x) {
+  deparse(substitute(x))
+}
+
+check1notboth <- function(x, y) {
+  if (is.null(x) && is.null(y)) {
+    stop(sprintf("One of %s or %s must be non-NULL", depsub(x), depsub(y)), call. = FALSE)
+  }
+  if (!is.null(x) && !is.null(y)) {
+    stop(sprintf("Supply only one of %s or %s", depsub(x), depsub(y)), call. = FALSE)
   }
 }
--- R/convert_units.R
+++ R/convert_units.R
@@ -12,7 +12,8 @@
 #' }
 
 convert_units <- function(udunits = NULL, ucum = NULL, url="http://coastwatch.pfeg.noaa.gov", ...){
-  args <- rc(list(UDUNITS=udunits, UCUM=ucum))
+  check1notboth(udunits, ucum)
+  args <- rc(list(UDUNITS = udunits, UCUM = ucum))
   res <- GET(paste0(pu(url), '/erddap/convert/units.txt'), query = args, ...)
   stop_for_status(res)
   content(res, "text")
