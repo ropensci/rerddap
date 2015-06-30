553dc78fcd0d3eb663010c15159baed2a6d8b8d5
Scott Chamberlain <myrmecocystus@gmail.com>
2015-06-30 13:21:28 -0700

    import base functions as needed fix #27

--- R/rerddap-package.r
+++ R/rerddap-package.r
@@ -20,6 +20,9 @@
 #' then you can increase the amount of data you get. See \code{\link{griddap}}
 #' for more details.
 #'
+#' @importFrom stats setNames
+#' @importFrom methods is
+#' @importFrom utils head read.csv read.delim URLencode
 #' @name rerddap-package
 #' @aliases rerddap
 #' @docType package
