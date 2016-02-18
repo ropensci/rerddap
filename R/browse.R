043a8c8fe9f7a5db17b742e632b02229444a51ca
Ben Tupper <btupper@bigelow.org>
2016-02-18 13:49:05 -0500

    add browse.R

---
+++ R/browse.R
@@ -0,0 +1,62 @@
+# browse.R
+
+#' Browse a dataset webpage.
+#'
+#' Note that it is an error to call this when \code{base::interactive()} returns FALSE
+#'
+#' @export
+#'
+#' @param x  datasetid or an object associated with a datasetid such \code{\link{info}}, \code{\link{griddap}} or \code{\link{tabledap}}
+#' @param url A URL for an ERDDAP server. Default: \url{http://upwell.pfeg.noaa.gov/erddap/}
+#' @param ... Further args passed on to \code{\link[httr]{BROWSE}} (must be a named parameter)
+#' @return the value returned by \code{\link[httr]{BROWSE}}
+#' @examples \dontrun{
+#'  ed_browse('noaa_esrl_28d5_ac3a_bb06')
+#'  }
+browse <- function(x, ...){
+    UseMethod("browse", x)
+}
+
+
+#' @export
+browse.character <- function(x, url = eurl(), ...){
+    stopifnot(interactive())
+    if (missing(x)) stop("datasetid is required")
+    uri <- sprintf(paste0(url, 'info/%s/index.html'), x)
+    httr::BROWSE(uri, ...)
+}
+
+#' @export
+browse.info <- function(x, ...){
+    datasetid <- attr(x, "datasetid")
+    browse(datasetid, ...)
+}
+
+#' @export
+browse.tabledap <- function(x, ...){
+    datasetid <- attr(x, "datasetid")
+    browse(datasetid, ...)
+}
+
+#' @export
+browse.griddap_nc <- function(x, ...){
+    datasetid <- attr(x, "datasetid")
+    browse(datasetid, ...)
+}
+
+
+#' @export
+browse.griddap_csv <- function(x, ...){
+    datasetid <- attr(x, "datasetid")
+    browse(datasetid, ...)
+}
+
+#' @export
+browse.default <- function(x, ...){
+    att <- attributes(x)
+    if (!is.null(att)){
+        if ('datasetid' %in% names(att)) browse(att, ...)
+    } else {
+      stop(sprintf("browse method not implemented for %s.", class(x)[1]))
+    }
+}
