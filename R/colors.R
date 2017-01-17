29ff1f71ff4cdd96e64c365dd631c233f57f982b
Roy Mendelssohn <roy.mendelssohn@noaa.gov>
2017-01-17 08:30:59 -0800

    Added new vignette, colors (#51)
    * Added new vignette, colors
    
    Added new vignette to the vignettes, and the file colors.Rda in the
    data folder, which is used for the color palettes
    
    * Many changes to new Vignette
    
    All requested edits done.
    Number of external packages used  sharply reduced.
    Several new examples included.
    Example netcdf file include in /inst/extdata, and successfully used in
    vignette.
    Info file for colors.rda included (colors.R)
    A lot of minor edits.
    
    * Labelling Fixes, Improved Data Credit
    
    Bugs in SODA and IMI titles fixed.  References to the original sources
    of the data for many of the datasets improved.

---
+++ R/colors.R
@@ -0,0 +1,19 @@
+#' cmocean colors
+#' The cmocean color palette by Kristen Thyng as implemented in the R package "oce"
+#'
+#' str(colors)
+#' List of 13
+#'  $ viridis
+#'  $ cdom
+#'  $ chlorophyll
+#'  $ density
+#'  $ freesurface
+#'  $ oxygen
+#'  $ par
+#'  $ phase
+#'  $ salinity
+#'  $ temperature
+#'  $ turbidity
+#'  $ velocity
+#'  $ vorticity
+"colors"
