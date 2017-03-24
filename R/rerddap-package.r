ad87f3a028163825b92dd4619dee4928dd4c70e6
Carson Sievert <cpsievert1@gmail.com>
2017-03-23 23:19:15 -0500

    cleanup NAMESPACE

--- R/rerddap-package.r
+++ R/rerddap-package.r
@@ -30,11 +30,16 @@
 #' @importFrom xml2 xml_text xml_find_all read_html
 #' @importFrom dplyr as_data_frame bind_rows
 #' @importFrom digest digest
-#' @importFrom grDevices extendrange
 #' @importFrom graphics plot.new
 #' @importFrom grid grid.newpage
-#' @importFrom ggplot2 ggplot geom_sf coord_sf theme_bw scale_color_gradientn scale_fill_gradientn ggtitle aes_string aes_
-#' @importFrom raster rasterFromXYZ
+#' @importFrom maps map
+#' @importFrom scales rescale col_numeric
+#' @importFrom lazyeval is_formula f_text f_eval
+#' @importFrom sf st_as_sf st_transform st_crs st_bbox st_graticule
+#' @importFrom ggplot2 ggplot geom_sf coord_sf theme_bw scale_color_gradientn
+#' scale_fill_gradientn ggtitle aes_string aes_
+#' @importFrom raster raster nlayers calc ncell nrow ncol crs extent resample
+#' projectRaster rasterToPolygons values plot brick setExtent isLonLat
 #' @name rerddap-package
 #' @aliases rerddap
 #' @docType package
